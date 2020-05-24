5c6303921debc0a8c552f8e8d1e8e4126c82f75b
kelletlen <vinczefanny@gmail.com>
2020-05-24 15:18:16 +0200

    new exercises

---
+++ week-01/day-4/src/DrawChessTable.java
@@ -0,0 +1,11 @@
+public class DrawChessTable {
+    public static void main(String[] args) {
+
+        for(int j=0; j<8; j++) {
+            for (int i=0; i<4; i++) {
+                System.out.print("% ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ week-01/day-4/src/GuessTheNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Guess the number:");
+        int a = scanner.nextInt();
+
+        int b = 7;
+
+
+        //if (a<b) {
+            //System.out.println("The stored number is higher");
+        //} else if (a>b) {
+            //System.out.println("The stored number is lower");
+       // } else {
+            //System.out.println("You found the number:" + b );
+       // }
+
+    }
+}
