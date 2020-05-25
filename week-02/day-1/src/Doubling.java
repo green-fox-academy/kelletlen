8bb5e294831db62e5862f06a090c862bc355c152
kelletlen <vinczefanny@gmail.com>
2020-05-25 16:46:08 +0200

    exercises

---
+++ week-02/day-1/src/Doubling.java
@@ -0,0 +1,14 @@
+public class Doubling {
+    public static void main(String[] args) {
+        int baseNum = 123;
+
+        System.out.println(Doubling(baseNum));
+
+    }
+    public static int Doubling(int a) {
+        return a * 2;
+
+    }
+
+
+}
---
+++ week-02/day-1/src/Greet.java
@@ -0,0 +1,13 @@
+public class Greet {
+    public static void main(String[] args) {
+        String al = "Green Fox";
+
+        greetFunction(al);
+
+
+    }
+    public static void greetFunction(String name) {
+        System.out.println("Greetings, dear " + name);
+
+    }
+}
