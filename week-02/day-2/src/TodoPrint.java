a8c9dd8781cd2efd1e4ea65943ec4db5ebe61e53
kelletlen <vinczefanny@gmail.com>
2020-05-26 17:57:54 +0200

    exercises

---
+++ week-02/day-2/src/TodoPrint.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+public class TodoPrint {
+    public static void main(String[] args) {
+        String todoText = " - Buy milk\n";
+
+        System.out.println(todoText.replace(" - ", "My todo:\n - ").replace("milk\n", "milk\n - Download games\n     - Diablo"));
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+
