46ba03f115b0b7f1a6f7edad854433f44ed76edd
kelletlen <vinczefanny@gmail.com>
2020-05-26 14:24:07 +0200

    exercises

---
+++ week-02/day-2/src/MapIntroduction.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+
+public class MapIntroduction {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
+
+        if(map.isEmpty()) {
+            System.out.println("Map is empty");
+        } else {
+            System.out.println("Map is not empty");
+        }
+        map.put(97,"a");
+        map.put(98, "b");
+        map.put(99, "c");
+        map.put(65, "A");
+        map.put(66, "B");
+        map.put(67, "C");
+
+        System.out.println(map.keySet());
+        System.out.println(map.values());
+
+        map.put(68, "D");
+
+        System.out.println(map.size());
+
+        System.out.println(map.get(99));
+
+        map.remove(97);
+
+        if(map.containsKey(100)) {
+            System.out.println("Map contains key ["+100+"]");
+        } else {
+            System.out.println("There isn't an associated value with key 100");
+        }
+
+        map.clear();
+
+
+    }
+
+}
