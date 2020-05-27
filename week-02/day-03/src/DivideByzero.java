5551e65b8ea5ceccd53ce29b30b6a2fe7bb715c2
kelletlen <vinczefanny@gmail.com>
2020-05-27 15:19:41 +0200

    exercises

---
+++ week-02/day-03/src/DivideByzero.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class DivideByzero {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter a number:");
+        int divisor = scanner.nextInt();
+
+        try {
+            int result = 10 / divisor;
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Can't divide by zero!");
+        }
+
+    }
+
+
+
+}
+
