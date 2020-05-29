35414a7129f6834e4dafa107dcf78e891c029875
kelletlen <vinczefanny@gmail.com>
2020-05-29 13:33:12 +0200

    exercises

--- week-02/day-4/src/FunctionToCenter.java
+++ week-02/day-4/src/FunctionToCenter.java
@@ -7,6 +7,30 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class FunctionToCenter {
     public static void mainDraw(Graphics graphics) {
 
+        int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < 17; i++ ) {
+            goToCenter(x, y, graphics);
+            x = 20*i;
+        }
+        for (int i= 0; i < 17; i++) {
+            goToCenter(x, y, graphics);
+            x =20*i;
+            y = 320;
+
+        }
+        for (int i=0; i < 17; i++) {
+            goToCenter(x, y, graphics);
+            x = 320;
+            y = 20*i;
+        }
+        for (int i=0; i<17; i++) {
+            goToCenter(x, y, graphics);
+            y = 20*i;
+            x = 0;
+        }
+
         // Create a function that draws a single line and takes 3 parameters:
         // The x and y coordinates of the line's starting point and the graphics
         // and draws a line from that point to the center of the canvas.
@@ -14,6 +38,14 @@ public class FunctionToCenter {
 
 
 
+    }
+
+    public static void goToCenter (int x, int y, Graphics g) {
+        int r = (int)(Math.random() * 255);
+        int green = (int)(Math.random() * 255);
+        int b = (int)(Math.random() * 255);
+        g.setColor(new Color(r, b, green,150));
+        g.drawLine(x, y, 160, 160);
     }
 
     // Don't touch the code below
