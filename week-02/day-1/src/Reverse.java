0dfc3edbf81cc9b5f1aa3fa9e3a683ee143fc79e
kelletlen <vinczefanny@gmail.com>
2020-06-03 20:25:57 +0200

    exercises

--- week-02/day-1/src/Reverse.java
+++ week-02/day-1/src/Reverse.java
@@ -2,9 +2,19 @@ public class Reverse {
     public static void main(String[] args) {
         int [] numbers = {3,4,5,6,7};
 
+        int temp = numbers[0];
+        numbers[0] = numbers[4];
+        numbers[4] = temp;
 
-        // for (int i =0; i<numbers.length; i++){
+        int temp2 = numbers [1];
+        numbers[1] = numbers [3];
+        numbers[3] = temp2;
 
-        //}
+
+
+        for (int i =0; i<numbers.length; i++){
+            System.out.print(numbers[i]);
+
+        }
     }
 }
