d634cca471809b128ff101b7c893b61bca5bc1bf
kelletlen <vinczefanny@gmail.com>
2020-06-03 17:05:30 +0200

    exercises

---
+++ week-02/day-1/src/Reverse.java
@@ -0,0 +1,10 @@
+public class Reverse {
+    public static void main(String[] args) {
+        int [] numbers = {3,4,5,6,7};
+
+
+        // for (int i =0; i<numbers.length; i++){
+
+        //}
+    }
+}
