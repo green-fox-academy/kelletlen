e8107b46b4a3ffb1f03f0aea3b02cc985d6e8a52
kelletlen <vinczefanny@gmail.com>
2020-06-03 14:33:38 +0200

    exercises

---
+++ week-02/day-1/src/Colors.java
@@ -0,0 +1,10 @@
+public class Colors {
+    public static void main(String[] args) {
+        String[][] colors = new String[][]{
+                {"lime", "forest green", "olive", "pale green", "spring green"},
+                {"orange red", "red", "tomato"},
+                {"orchid", "violet", "pink", "hot pink"}
+        };
+
+    }
+}
