81588ab7fd445c5bd9411d2f62082b4371c5411a
kelletlen <vinczefanny@gmail.com>
2020-06-03 15:37:36 +0200

    exercises

---
+++ week-02/day-1/src/SwapElements.java
@@ -0,0 +1,12 @@
+public class SwapElements {
+    public static void main(String[] args) {
+        String [] orders = {"first", "second", "third"};
+
+        String temp = orders[0];
+        orders[0] = orders [2];
+        orders[2] = temp;
+
+        System.out.println(orders[0] + "," + orders [2]);
+    }
+
+}
