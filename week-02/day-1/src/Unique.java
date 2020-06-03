0a79021ebe3e76e104f1c99d038f87b8dde44eba
kelletlen <vinczefanny@gmail.com>
2020-06-03 22:09:09 +0200

    exercises

---
+++ week-02/day-1/src/Unique.java
@@ -0,0 +1,15 @@
+public class Unique {
+
+    //  Create a function that takes a list of numbers as a parameter
+    //  Returns a list of numbers where every number in the list occurs only once
+
+    //  Example
+    //System.out.println(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34}));
+    //  should print: `[1, 11, 34, 52, 61]`
+
+    public static void unique (int [] numbers) {
+
+        // visszatérő érték kell
+        // csinálni kell egy másik arrayt, és if/else-szel ha már benne van kihagyja
+    }
+}
