a88a3d8bd2b3cee8e0be9f4ef6cfff41e5f0d55a
kelletlen <vinczefanny@gmail.com>
2020-06-06 15:07:36 +0200

    exercises

--- week-02/day-1/src/Unique.java
+++ week-02/day-1/src/Unique.java
@@ -7,7 +7,11 @@ public class Unique {
     //System.out.println(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34}));
     //  should print: `[1, 11, 34, 52, 61]`
 
-    public static void unique (int [] numbers) {
+    public static int [] unique (int [] numbers) {
+
+       // for (int i = 0; i < numbers.length; i++) {
+
+     //   }
 
         // visszatérő érték kell
         // csinálni kell egy másik arrayt, és if/else-szel ha már benne van kihagyja
--- week-02/day-2/src/PersonalFinance.java
+++ week-02/day-2/src/PersonalFinance.java
@@ -1,15 +1,1 @@
-import java.util.ArrayList;
 
-public class PersonalFinance {
-    public static void main(String [] args) {
-        ArrayList<Integer> expenses = new ArrayList<>();
-
-        expenses.add(500);
-        expenses.add(1000);
-        expenses.add(1250);
-        expenses.add(175);
-        expenses.add(800);
-        expenses.add(120);
-    }
-
-    }
