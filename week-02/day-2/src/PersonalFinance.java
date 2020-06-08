6618cc891b7951b5f375dd5f9895b8248d0cbeec
kelletlen <vinczefanny@gmail.com>
2020-06-08 14:20:08 +0200

    exercises

--- week-02/day-2/src/PersonalFinance.java
+++ week-02/day-2/src/PersonalFinance.java
@@ -1,1 +1,33 @@
+import java.util.Arrays;
+
+public class PersonalFinance {
+  public static void main(String[] args) {
+    int[] expenses =  new int [] {500, 1000, 1250, 175, 800, 120};
+
+    int sum = 0;
+
+    for (int i = 0; i < expenses.length; i++) {
+      sum += expenses[i];
+
+    }
+    System.out.println("We spent: " + sum);
+
+    Arrays.sort(expenses);
+
+    int greatestExpense = expenses[expenses.length -1];
+
+    System.out.println("Our greatest expense: " + greatestExpense);
+
+
+    int cheapest = expenses [0];
+
+
+    System.out.println("Our cheapest spending was: " + cheapest);
+
+    int average = sum / expenses.length;
+
+    System.out.println("The average amount of our spending was: " + average);
+
+    }
+  }
 
---
+++ week-02/day-2/src/telephoneBook.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class telephoneBook {
+  public static void main(String[] args) {
+    HashMap<String, String> telephoneBook = new HashMap<>();
+
+    telephoneBook.put("William A. Lathan","405-709-1865");
+    telephoneBook.put("John K. Miller","402-247-8568");
+    telephoneBook.put("Hortensia E. Foster","606-481-6467");
+    telephoneBook.put("Amanda D. Newland","319-243-5613");
+    telephoneBook.put("Brooke P. Askew","307-687-2982");
+
+    System.out.println(telephoneBook.get("John K. Miller"));
+
+    for (Map.Entry<String, String> entry: telephoneBook.entrySet()) {
+      if (entry.getValue().equals ("307-687-2982")) {
+        System.out.println(entry.getKey());
+      }
+    }
+
+    System.out.println(telephoneBook.containsKey("Chris E. Myers"));
+
+
+  }
+}
