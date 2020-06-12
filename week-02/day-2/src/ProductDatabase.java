ead6fade5340f836dcd59d6f2bdee668a03f5aa1
kelletlen <vinczefanny@gmail.com>
2020-06-12 19:42:21 +0200

    finished ProductDatabase

--- week-02/day-2/src/ProductDatabase.java
+++ week-02/day-2/src/ProductDatabase.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.IntStream;
+import java.util.List;
 
 public class ProductDatabase {
   public static void main(String[] args) {
@@ -12,13 +14,10 @@ public class ProductDatabase {
     productDatabase.put("Bread", 50);
     productDatabase.put("Chicken", 550);
 
-    System.out.println(productDatabase.get("Fish"));
+    System.out.println("The price of the fish is: " + productDatabase.get("Fish"));
 
     Object[] array = productDatabase.values().toArray();
 
-    /*for (int i = 0; i < array.length; i++) {
-      System.out.println(array[i]);
-    }*/
     Integer[] prices = new Integer[array.length];
 
     for (int i = 0; i < array.length; i++) {
@@ -40,13 +39,33 @@ public class ProductDatabase {
     int average = sum/prices.length;
     System.out.println("The average price is: " + average);
 
+    int below300 = 0;
 
+    for (int i = 0; i < prices.length; i++) {
+      if (prices[i]  < 300) {
+        below300 = below300 +1;
+      }
+    }
+    System.out.println("Products below 300: " + below300);
 
+   List<Integer> pricesList = Arrays.asList(prices);
 
+   if(pricesList.contains(125)) {
+     System.out.println("Yes, there is something we can buy for exactly 125.");
+   } else {
+     System.out.println("No, we can't buy anything for exactly 125.");
+   }
 
+   int cheapest = prices[0];
 
+    for (Map.Entry<String, Integer> entry: productDatabase.entrySet()) {
+      if (entry.getValue().equals (cheapest)) {
+        System.out.println("The cheapest product is: " + entry.getKey());
+      }
+    }
   }
 }
+
   /* Create an application which solves the following problems.
 
     How much is the fish?
