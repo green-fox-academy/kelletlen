126f2a06def0971de63d621adaaaae66569c4dc7
kelletlen <vinczefanny@gmail.com>
2020-06-17 19:29:10 +0200

    added Doubled

---
+++ week-02/day-3/src/Doubled.java
@@ -0,0 +1,33 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Doubled {
+  public static void main(String[] args) {
+    // Create a method that decrypts the duplicated-chars.txt
+    try {
+      Path filePath = Paths.get("assets/duplicated-chars.txt");
+      List<String> content = Files.readAllLines(filePath);
+      List<String> newContent = new ArrayList<>();
+      Path filePath2 = Paths.get("assets/chars.txt");
+      StringBuilder sb = new StringBuilder();
+      for (int i = 0; i < content.size() ; i++) {
+        String line = content.get(i);
+        for (int j = 0; j < line.length(); j++) {
+          if (j%2 == 0) {
+            sb.append(line.charAt(j));
+
+          }
+        }
+      }
+      newContent.add(sb.toString().replace(".", ".\n").replace("Python", "python\n").replace("Peters", "Peters\n\n"));
+      Files.write(filePath2, newContent);
+    } catch (Exception e) {
+      System.out.println("fail");
+    }
+
+
+  }
+}
