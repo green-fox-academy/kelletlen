4c7fda0663889490476e088995783c7c93816458
kelletlen <vinczefanny@gmail.com>
2020-06-20 15:28:41 +0200

    added sharpie

---
+++ week-05/day-3/src/sharpie/Sharpie.java
@@ -0,0 +1,16 @@
+package sharpie;
+
+public class Sharpie {
+  private String color;
+  private float width;
+  private float inkAmount;
+
+  public Sharpie(String color, float width, float inkAmount) {
+    this.color = color;
+    this.width = width;
+    this.inkAmount = 100;
+  }
+  public void use () {
+    inkAmount -=1 ;
+  }
+}
