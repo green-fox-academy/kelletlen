bf684fc325494481f1075c7038c0acc8e6e6dda3
kelletlen <vinczefanny@gmail.com>
2020-06-25 23:21:48 +0200

    added teacherStudent

---
+++ week-05/day-3/src/teacherstudent/Student.java
@@ -0,0 +1,11 @@
+package teacherstudent;
+
+public class Student {
+  public void learn() {
+    System.out.println("The student is learning something new.");
+  }
+  public void question(Teacher teacher) {
+    teacher.answer();
+
+  }
+}
