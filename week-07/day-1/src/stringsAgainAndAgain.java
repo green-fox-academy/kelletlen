e1e026c12ea578879b3714e6c6368e3671eed910
kelletlen <vinczefanny@gmail.com>
2020-07-03 17:27:40 +0200

    added stringAgainandagain and fibonacci

---
+++ week-07/day-1/src/stringsAgainAndAgain.java
@@ -0,0 +1,12 @@
+public class stringsAgainAndAgain {
+  public static void main(String[] args) {
+    //Given a string, compute recursively a new string where all the adjacent chars are now separated by a *
+    System.out.println(separate("asdasdasdasd"));
+  }
+  public static String separate (String input) {
+    if (input.isEmpty()) return "";
+    char c = input.charAt(0);
+    return c + '*' + separate(input.substring(1));
+    }
+}
+// it's printing numbers instead of string
