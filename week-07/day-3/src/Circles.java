e6008427733701584035bafb9f771cbb43b6d9d0
kelletlen <vinczefanny@gmail.com>
2020-07-04 17:13:11 +0200

    added Sierpinsky carpet

--- week-07/day-3/src/Circles.java
+++ week-07/day-3/src/Circles.java
@@ -6,15 +6,17 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class Circles {
   public static void mainDraw(Graphics graphics){
 
-  // graphics.drawOval(0, 0, WIDTH, HEIGHT);
+   graphics.drawOval(0, 0, WIDTH, HEIGHT);
+   graphics.drawOval(WIDTH/4, 0,WIDTH/2, HEIGHT/2 );
+   graphics.drawOval(0, ((WIDTH/8)*3), WIDTH/2, HEIGHT/2);
 
 
 
   }
 
   // Don't touch the code below
-  static int WIDTH = 320;
-  static int HEIGHT = 320;
+  static int WIDTH = 600;
+  static int HEIGHT = 600;
 
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
---
+++ week-07/day-3/src/SierpinskyCarpet.java
@@ -0,0 +1,66 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+public class SierpinskyCarpet {
+  private static final int LIMIT = 4;
+  public static void mainDraw(Graphics graphics) {
+
+    int x = WIDTH / 3;
+    int y = HEIGHT / 3;
+    int size = WIDTH / 3;
+
+    drawSierpienskyCarpet(graphics, x, y, size, 0);
+
+    /* graphics.fillRect(x, y, width, height);
+    graphics.fillRect(x - ((width / 3) * 2), y - ((height / 3) * 2), width / 3, height / 3);
+    graphics.fillRect(x + (x / 3), y - ((height / 3) * 2), width / 3, height / 3);
+    graphics.fillRect((2 * width) + (width / 3), y - ((height / 3) * 2), width / 3, height / 3);
+    graphics.fillRect(x - ((width / 3) * 2), y + (y / 3), width / 3, height / 3);
+    graphics.fillRect(x - ((width / 3) * 2), (2 * height) + (height / 3), width / 3, height / 3);
+    graphics.fillRect(x + (x / 3), (2 * height) + (height / 3), width / 3, height / 3);
+    graphics.fillRect((2 * width) + (width / 3), (2 * height) + (height / 3), width / 3, height / 3);
+    graphics.fillRect((2 * width) + (width / 3), height + (height / 3), width / 3, height / 3); */
+
+
+  }
+  public static void drawSierpienskyCarpet (Graphics graphics, int x, int y, int size, int counter){
+    if (counter > LIMIT) {
+      return;
+    }
+    graphics.fillRect(x, y, size, size);
+    drawSierpienskyCarpet(graphics, x - ((size / 3) * 2), y - ((size / 3) * 2), size / 3, counter+1);
+    drawSierpienskyCarpet(graphics, x + (size / 3), y - ((size / 3) * 2), size / 3, counter+1);
+    drawSierpienskyCarpet(graphics, x + (size + size/3), y - ((size / 3) * 2), size / 3, counter+1);
+    drawSierpienskyCarpet(graphics,x - ((size / 3) * 2), y + (size / 3), size / 3, counter+1);
+    drawSierpienskyCarpet(graphics, x - ((size / 3) * 2), y + (size + size/3), size / 3, counter+1);
+    drawSierpienskyCarpet(graphics,x + (size / 3), y + (size + size/3), size / 3, counter+1 );
+    drawSierpienskyCarpet(graphics, x + (size + size/3), y + (size + size/3), size / 3, counter+1);
+    drawSierpienskyCarpet(graphics, x + (size + size/3), y + (size / 3), size / 3, counter+1);
+
+  }
+
+  // Don't touch the code below
+  static int WIDTH = 600;
+  static int HEIGHT = 600;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    ImagePanel panel = new ImagePanel();
+    panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.add(panel);
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+    jFrame.pack();
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
