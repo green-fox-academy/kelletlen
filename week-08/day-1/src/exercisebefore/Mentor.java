b4944328688e0462e35ae1e5526d0aae05c27467
kelletlen <vinczefanny@gmail.com>
2020-07-06 15:49:01 +0200

    added exercise before inheritance

---
+++ week-08/day-1/src/exercisebefore/Mentor.java
@@ -0,0 +1,28 @@
+package exercisebefore;
+
+public class Mentor {
+  private String name;
+  private int age;
+  private String gender;
+  private String level;
+
+  public Mentor(String name, int age, String gender, String level) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.level = level;
+  }
+
+  public Mentor() {
+    this.name = "Jane Doe";
+    this.age = 30;
+    this.gender = "female";
+    this.level = "intermediate";
+  }
+  public void getGoal () {
+    System.out.println("Educate brilliant junior software developers.");
+  }
+  public void introduce () {
+    System.out.println("Hi, I'm " + name + ", a " + age + " year old " + gender + level + " mentor.");
+  }
+}
---
+++ week-08/day-1/src/exercisebefore/Person.java
@@ -0,0 +1,26 @@
+package exercisebefore;
+
+public class Person {
+  private String name;
+  private int age;
+  private String gender;
+
+  public Person(String name, int age, String gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  public Person() {
+    this.name = "Jane Doe";
+    this.age = 30;
+    this.gender = "female";
+  }
+
+  public void introduce() {
+    System.out.println("Hi, I'm " + name + ", a " + age + " year old " + gender + ".");
+  }
+  public void getGoal() {
+    System.out.println("My goal is: live for the moment!");
+  }
+}
