18c0fdba084bba9a89fab0268d16417ca10fc09b
kelletlen <vinczefanny@gmail.com>
2020-08-12 17:34:13 +0200

    added exercise 3 & 4

---
+++ week-13/day-1/src/streams/Exercise4.java
@@ -0,0 +1,18 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+
+public class Exercise4 {
+  public static void main(String[] args) {
+
+    // Write a Stream Expression to get the average value of the odd numbers from the following list:
+
+    List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+
+    IntSummaryStatistics stats = numbers.stream().filter(p -> p%2 ==1).mapToInt((p) ->p).summaryStatistics();
+    System.out.println(stats.getAverage());
+
+  }
+}
