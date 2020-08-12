e9d8b243e70121eb1d2eff7aaa94de18f7c8bbba
kelletlen <vinczefanny@gmail.com>
2020-08-12 17:00:34 +0200

    added exercise 1 & 2

---
+++ week-13/day-1/src/streams/Exercise2.java
@@ -0,0 +1,15 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercise2 {
+  public static void main(String[] args) {
+    //Write a Stream Expression to get the squared value of the positive numbers from the following list:
+
+    List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+
+    numbers.stream().filter(p -> p > 0).map(p -> p*p).forEach(System.out::println);
+  }
+}
+
