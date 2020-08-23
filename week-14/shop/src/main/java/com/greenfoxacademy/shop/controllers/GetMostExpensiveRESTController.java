6237a7a1b9872dd6ef120ae27da3dec328d4f358
kelletlen <vinczefanny@gmail.com>
2020-08-23 14:56:18 +0200

    amended MostExpensive and GetAverage controllers

--- week-14/shop/src/main/java/com/greenfoxacademy/shop/controllers/GetMostExpensiveRESTController.java
+++ week-14/shop/src/main/java/com/greenfoxacademy/shop/controllers/GetMostExpensiveRESTController.java
@@ -1,14 +1,17 @@
 package com.greenfoxacademy.shop.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class GetMostExpensiveRESTController {
   ItemList itemList = new ItemList();
 
   @RequestMapping(value="/webshop/mostexpensive")
-  public String getMostExpensive () {
-    return itemList.getMostExpensive();
+  public String getMostExpensive (Model model) {
+    model.addAttribute("item", "Most expensive item: " + itemList.getMostExpensive());
+    return "forSingleData";
   }
 }
--- week-14/shop/src/main/java/com/greenfoxacademy/shop/controllers/StockAverageRESTController.java
+++ week-14/shop/src/main/java/com/greenfoxacademy/shop/controllers/StockAverageRESTController.java
@@ -1,15 +1,17 @@
 package com.greenfoxacademy.shop.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class StockAverageRESTController {
   ItemList itemList = new ItemList();
 
   @RequestMapping(value="/webshop/stockaverage")
-  public double getStockAverage () {
-    return itemList.getStockAverage();
+  public String getStockAverage (Model model) {
+      model.addAttribute("item", "Average stock:" + itemList.getStockAverage());
+    return "forSingleData";
   }
 }
