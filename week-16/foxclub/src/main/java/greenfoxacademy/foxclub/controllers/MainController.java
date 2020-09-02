f2c7b01729f035aa45eae52ba7afcdf63e77fc67
kelletlen <vinczefanny@gmail.com>
2020-09-02 21:03:09 +0200

    added login page, created fox class

--- week-16/foxclub/src/main/java/greenfoxacademy/foxclub/controllers/MainController.java
+++ week-16/foxclub/src/main/java/greenfoxacademy/foxclub/controllers/MainController.java
@@ -1,5 +1,6 @@
 package greenfoxacademy.foxclub.controllers;
 
+import greenfoxacademy.foxclub.models.Fox;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,20 +8,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+
 @Controller
 public class MainController {
+  ArrayList<Fox> foxes = new ArrayList<>();
 
   @GetMapping(path="/")
-  public String show () {
-    return "index";
+  public String show (Model model, @RequestParam(name="name", required = false) String name) {
+    if (name == null || name.isEmpty()) {
+      return "redirect:/login";
+    } else {
+      model.addAttribute("name", name);
+      return "index";
+    }
   }
+
   @GetMapping(path="/login")
   public String getLogin () {
+
     return "login";
   }
+
   @PostMapping(path="/login")
-  public String postLogin (Model model, @RequestParam(name="name") String name) {
-    model.addAttribute("name", name);
-    return "login";
+  public String postLogin (@RequestParam(name="name") String name) {
+    return "redirect:/?name=" + name;
   }
 }
---
+++ week-16/foxclub/src/main/java/greenfoxacademy/foxclub/models/Fox.java
@@ -0,0 +1,32 @@
+package greenfoxacademy.foxclub.models;
+
+import java.util.ArrayList;
+
+public class Fox {
+  private String name;
+  private String food;
+  private String drink;
+  private ArrayList<String> tricks;
+
+  public Fox(String name, String food, String drink) {
+    this.name = name;
+    this.food = food;
+    this.drink = drink;
+    this.tricks = new ArrayList<>();
+  }
+  public String getName() {
+    return name;
+  }
+
+  public String getFood() {
+    return food;
+  }
+
+  public String getDrink() {
+    return drink;
+  }
+
+  public ArrayList<String> getTricks() {
+    return tricks;
+  }
+}
