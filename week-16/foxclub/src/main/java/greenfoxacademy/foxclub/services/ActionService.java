9639b60141216cd8c40d584280736a1af1d1934a
kelletlen <vinczefanny@gmail.com>
2020-09-09 22:17:54 +0200

    added current time to actions

--- week-16/foxclub/src/main/java/greenfoxacademy/foxclub/services/ActionService.java
+++ week-16/foxclub/src/main/java/greenfoxacademy/foxclub/services/ActionService.java
@@ -1,16 +1,20 @@
 package greenfoxacademy.foxclub.services;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 @Service
 public class ActionService {
   ArrayList<String> actionList = new ArrayList<>();
+  Date date = new Date();
+  SimpleDateFormat formatter = new SimpleDateFormat("yyyy. MMMM dd. HH:mm:ss");
 
   public ArrayList<String> getActionList () {
     return actionList;
   }
   public void addToList (String action) {
-    actionList.add(action);
+    actionList.add(formatter.format(date) + " : " + action);
   }
 }
