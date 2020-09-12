60fd2872921fc8cef99879f683b75c3965e90657
kelletlen <vinczefanny@gmail.com>
2020-09-12 16:10:16 +0200

    added listing todos from mysql

---
+++ week-17/mysql/src/main/java/com/greenfoxacademy/mysql/MysqlApplication.java
@@ -0,0 +1,33 @@
+package com.greenfoxacademy.mysql;
+
+import com.greenfoxacademy.mysql.models.Todo;
+import com.greenfoxacademy.mysql.repositories.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlApplication implements CommandLineRunner {
+
+  final
+  TodoRepository todoRepository;
+
+  public MysqlApplication(TodoRepository todoRepository) {
+    this.todoRepository = todoRepository;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(MysqlApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    todoRepository.save(new Todo("make the beds", true));
+    todoRepository.save(new Todo("do the washing up", false));
+    todoRepository.save(new Todo("clean the bathroom", false));
+    todoRepository.save(new Todo("tidy up", true));
+    todoRepository.save(new Todo("mop", true));
+     
+  }
+}
---
+++ week-17/mysql/src/main/java/com/greenfoxacademy/mysql/controllers/TodoController.java
@@ -0,0 +1,29 @@
+package com.greenfoxacademy.mysql.controllers;
+
+
+import com.greenfoxacademy.mysql.repositories.TodoRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("todo")
+public class TodoController {
+
+  final
+  TodoRepository todoRepository;
+
+  public TodoController(TodoRepository todoRepository) {
+    this.todoRepository = todoRepository;
+  }
+
+  @GetMapping(value = {"list", "/"})
+  public String list(Model model) {
+    model.addAttribute("todos", todoRepository.findAll());
+    return "todoList";
+
+
+
+  }
+}
