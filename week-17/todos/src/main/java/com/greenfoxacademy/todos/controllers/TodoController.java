bdda76321c866accbf91fbe44683130d674b5bbf
kelletlen <vinczefanny@gmail.com>
2020-09-26 12:30:27 +0200

    fixed todo app

--- week-17/todos/src/main/java/com/greenfoxacademy/todos/controllers/TodoController.java
+++ week-17/todos/src/main/java/com/greenfoxacademy/todos/controllers/TodoController.java
@@ -1,10 +1,15 @@
 package com.greenfoxacademy.todos.controllers;
 
+import com.greenfoxacademy.todos.models.Todo;
 import com.greenfoxacademy.todos.repositories.TodoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("todo")
@@ -18,8 +23,30 @@ public class TodoController {
   }
 
   @GetMapping(value = {"list", "/"})
-  public String list(Model model) {
-    model.addAttribute("todos", todoRepository.findAll());
+  public String list(Model model, @RequestParam(name="isActive", defaultValue = "false") boolean isActive) {
+    if (isActive) {
+      ArrayList<Todo> actives = new ArrayList();
+      for (Todo item : todoRepository.findAll()) {
+        if (!item.isDone()) {
+          actives.add(item);
+        }
+      }
+      model.addAttribute("todos", actives);
+    } else {
+      model.addAttribute("todos", todoRepository.findAll());
+    }
     return "todoList";
+  }
+
+  @GetMapping(value="add")
+  public String addTask () {
+    return "add";
+  }
+
+  @PostMapping(value="add")
+  public String postAddTask (@RequestParam(name="task") String task, @RequestParam(name="urgency") boolean urgency) {
+    Todo newTodo = new Todo(task, urgency);
+    todoRepository.save(newTodo);
+    return "redirect:/todo/list?isActive=false";
   }
 }
