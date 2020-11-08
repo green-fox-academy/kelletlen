7917c586d22972afe3661b9210d2592c929637a2
kelletlen <vinczefanny@gmail.com>
2020-11-08 12:03:36 +0100

    add everything

--- week-25/movie-app/src/main/java/com/greenfoxacademy/movieapp/MovieInterface.java
+++ week-25/movie-app/src/main/java/com/greenfoxacademy/movieapp/MovieInterface.java
@@ -3,12 +3,14 @@ package com.greenfoxacademy.movieapp;
 import org.springframework.web.bind.annotation.PostMapping;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 import java.util.List;
 
 public interface MovieInterface {
   @GET("/movies")
+  @Headers("Content-Type: application/json;charset=utf-8")
   Call<List<Movie>> getMovies();
 
 }
