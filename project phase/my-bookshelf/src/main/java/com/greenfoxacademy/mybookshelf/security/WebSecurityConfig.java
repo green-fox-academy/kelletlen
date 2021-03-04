53af2f65e4c1a4f2d189d6441f0509fe09622e1a
kelletlen <vinczefanny@gmail.com>
2021-03-04 16:28:47 +0100

    authentication set up, able to register

---
+++ project phase/my-bookshelf/src/main/java/com/greenfoxacademy/mybookshelf/security/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.greenfoxacademy.mybookshelf.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private final JwtTokenFilter jwtTokenFilter;
+
+  public WebSecurityConfig(JwtTokenFilter jwtTokenFilter) {
+    this.jwtTokenFilter = jwtTokenFilter;
+  }
+
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+        .withUser("user1").password("pw").roles("");
+
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable().authorizeRequests()
+        .antMatchers(HttpMethod.POST, "/users/register").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+  }
+}
